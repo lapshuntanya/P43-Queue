152ea61e86588e4a0686bced138f0ce4479323b6
Tetiana Perederii <lapshun.tatiana@gmail.com>
2025-12-03 17:41:43 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
+#include "MyQueue.h"
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
 
+    myQueue::Queue<int> nums;
+    nums.show();
+
+    nums.push_back(56);
+    nums.push_back(23);
+    nums.push_back(-356);
+    nums.push_back(9);
+    nums.show();
+
+    //обробка черги
+    while (!nums.isEmpty()) {
+        nums.show();
+        cout << "First: " << nums.first() << endl;
+        nums.pop_front();
+        cout << endl;
+    }
+    nums.show();
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
